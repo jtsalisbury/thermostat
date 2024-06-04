36ed66888ad6c228297b839acb9daeb25485a30a
JT Salisbury <crazyscouter1@gmail.com>
2024-06-04 19:13:13 -0400

    working

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ export const readTemperature = () => {
     const i2c1 = i2c.openSync(1);
     const rawData = i2c1.readWordSync(ADDR, TEMP_REG);
     console.log(rawData);
+
+    const temp = ((175.72 * rawData) / 65536.0) - 46.85
+    console.log(temp);
+
     i2c1.closeSync();
 
     return rawData;
