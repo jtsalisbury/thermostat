d6d1eac6273b88d61a7e85c3c7a49b342236e3cf
JT Salisbury <crazyscouter1@gmail.com>
2026-02-07 15:47:55 -0500

    latest

--- index.js
+++ index.js
@@ -1,37 +1,98 @@
-import { readTemperature } from "./temperature.js";
-import fs from 'fs';
+import {
+    LedMatrix,
+    GpioMapping,
+    LedMatrixUtils,
+    PixelMapperType,
+} from 'rpi-led-matrix';
 
-const SETTINGS_FILE = 'settings.json';
-const DATA_PREFIX = '_temp.json';
+import fetch from 'node-fetch';
 
-const date = new Date();
-const month = date.toLocaleString('default', { month: 'long' });
+const matrix = null;
 
-const dataPath = month + DATA_PREFIX;
+const setupMatrix = () => {
 
-let currentData = [];
-if (fs.existsSync(dataPath)) {
-    currentData = JSON.parse(fs.readFileSync(dataPath));
+
+matrix = new LedMatrix(
+    {
+        ...LedMatrix.defaultMatrixOptions,
+        rows: 32,
+        cols: 64,
+        chainLength: 2,
+        hardwareMapping: GpioMapping.AdafruitHatPwm,
+        pixelMapperConfig: LedMatrixUtils.encodeMappers({
+            type: PixelMapperType.U,
+        }),
+    },
+    {
+        ...LedMatrix.defaultRuntimeOptions,
+        gpioSlowdown: 1,
+    }
+);
 }
 
-let intervalId;
-const enableThermostat = () => {
-    intervalId = setInterval(() => {
-        const data = readTemperature();
-        const now = new Date();
+const getArrivalData = async () => {
+    try {
+        const response = await fetch('https://train-arrivals-6c5c64469c48.herokuapp.com/v1/arrivals');
+        const data = await response.json();
 
-        currentData.push({
-            date: now,
-            temp: data
-        });
+        return data;
 
-        fs.writeFileSync(dataPath, JSON.stringify(currentData));
+    } catch (error) {
+        console.error('Fetch error:', error);
 
-    }, 60 * 1000);
+        return null;
+    }
 }
 
-const disableThermostat = () => {
-    clearInterval(intervalId);
+
+
+const renderData = async () => {
+    const data = await getArrivalData();
+
+    if (data == null) {
+        return;
+    }
+
+    if (data.trains == null || data.trains.length === 0) {
+        showNoTrains();
+    } else {
+        
+        if (data.trains.length > 3) {
+            renderArrivalTimes(data.trains.slice(0, 3));
+        } else {
+            renderArrivalTimes(data.trains);
+        }
+
+    }
 }
 
-enableThermostat();
+const getColor = (train) => {
+    if (train.train_type == "J" || train.train_type == "Z") {
+        return 0xA52A2A
+
+    } else if (train.train_type == "M") {
+        return 0xFF6320;
+    }
+}
+
+const showNoTrains = () => {
+
+}
+
+const renderArrivalTimes = (trains) => {
+    console.log(trains);
+}
+
+
+const start = () => {
+    //setupMatrix();
+    
+    setInterval(() => {
+        renderData();
+    }, 4000);
+}
+
+
+(() => {
+    start();
+})();
